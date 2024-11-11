d4bf81b46e87a6e71618a9059674d97cbd817f34
jothiprakash06 <jothi.prakash@agrizy.in>
2024-11-11 16:46:19 +0530

    Integrating jenkins and Docker

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,35 @@
 import { BrowserRouter } from "react-router-dom";
-import {About,Contact,Experience,Hero,Navbar,Tech,Works,StarsCanvas} from './components';
-import { Feedback } from "@material-ui/icons";
+import {
+  About,
+  Contact,
+  Experience,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
 const App = () => {
   return (
     <div>
       <BrowserRouter>
-      <div className='relative z-0 bg-primary'>
-        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
-            <Navbar/>
-            <Hero/>
+        <div className="relative z-0 bg-primary">
+          <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+            <Navbar />
+            <Hero />
           </div>
           <About />
-        <Experience />
-        <Tech />
-        <Works />
-        <div className='relative z-0'>
-        <Contact/>
-        <StarsCanvas/>
-        </div>
+          <Experience />
+          <Tech />
+          <Works />
+          <div className="relative z-0">
+            <Contact />
+            <StarsCanvas />
+          </div>
         </div>
       </BrowserRouter>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
